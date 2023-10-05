c6626d77deabaee2ae0e0d3de05dee820fa87bf2
Evgenii Ermak <djermak3000@mail.ru>
2023-10-05 20:11:13 +0300

    homework # 1 done 2.0 - comments and shortcomings have been corrected

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
-import requests
+import os
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -6,30 +7,51 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.api_key = 'AIzaSyAqyYxiqs3S2UnchCBECOAFigXA_PJqAwY'
+        self.api_key = os.getenv('YT_API_KEY')
+        if not self.api_key:
+            raise ValueError("API ключ не найден в переменных окружения.")
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        url = f'https://www.googleapis.com/youtube/v3/channels'
-        params = {
-            'part': 'snippet,statistics',
-            'id': self.channel_id,
-            'key': self.api_key,
-        }
+    def get_channel_info(self) -> dict:
+        """Получает информацию о канале с помощью YouTube API и возвращает ее в виде словаря."""
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
 
-        response = requests.get(url, params=params)
-        data = response.json()
+        response = youtube.channels().list(
+            part='snippet,statistics',
+            id=self.channel_id
+        ).execute()
 
-        if 'items' in data:
-            channel_data = data['items'][0]
+        if 'items' in response:
+            channel_data = response['items'][0]
 
             snippet = channel_data['snippet']
             statistics = channel_data['statistics']
 
-            print(f"Название канала: {snippet['title']}")
-            print(f"Описание: {snippet['description']}")
-            print(f"Подписчики: {statistics['subscriberCount']}")
-            print(f"Просмотры: {statistics['viewCount']}")
-            print(f"Количество видео: {statistics['videoCount']}")
+            channel_info = {
+                "Название канала": snippet['title'],
+                "Описание": snippet['description'],
+                "Подписчики": statistics['subscriberCount'],
+                "Просмотры": statistics['viewCount'],
+                "Количество видео": statistics['videoCount']
+            }
+            return channel_info
+        else:
+            return None
+
+    def print_info(self) -> None:
+        """Выводит информацию о канале в консоль без лишних скобок и кавычек."""
+        channel_info = self.get_channel_info()
+
+        if channel_info:
+            print("Информация о канале:")
+            print(f"Название канала: {channel_info['Название канала']}")
+            print(f"Описание: {channel_info['Описание']}")
+            print(f"Подписчики: {channel_info['Подписчики']}")
+            print(f"Просмотры: {channel_info['Просмотры']}")
+            print(f"Количество видео: {channel_info['Количество видео']}")
         else:
             print("Канал не найден или произошла ошибка при получении данных.")
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
