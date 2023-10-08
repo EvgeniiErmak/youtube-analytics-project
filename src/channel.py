294c8c658fd0713a10ba7fe81c2ada2961f77cc0
Evgenii Ermak <djermak3000@mail.ru>
2023-10-08 20:26:57 +0300

    homework 3 done — upgrade channel.py

--- src/channel.py
+++ src/channel.py
@@ -2,89 +2,78 @@ import json
 import os
 from googleapiclient.discovery import build
 
-
 class Channel:
-    def __init__(self, channel_id: str):
-        self.channel_id = channel_id
-        self.api_key = os.getenv('YT_API_KEY')
-        if not self.api_key:
-            raise ValueError("API ключ не найден в переменных окружения.")
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        self._init_from_api()
 
-        # Инициализируем атрибуты данными из YouTube API
-        self.channel_info = self.get_channel_info()
-
-    def get_channel_info(self):
+    def _init_from_api(self) -> None:
         youtube = self.get_service()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
-        response = youtube.channels().list(
-            part='snippet,statistics',
-            id=self.channel_id
-        ).execute()
+        if 'items' in channel:
+            channel_data = channel['items'][0]['snippet']
+            statistics = channel['items'][0]['statistics']
 
-        if 'items' in response:
-            channel_data = response['items'][0]
-
-            snippet = channel_data['snippet']
-            statistics = channel_data['statistics']
-
-            channel_info = {
-                "id канала": self.channel_id,
-                "Название канала": snippet['title'],
-                "Описание канала": snippet['description'],
-                "Ссылка на канал": f"https://www.youtube.com/channel/{self.channel_id}",
-                "Количество подписчиков": statistics['subscriberCount'],
-                "Количество видео": statistics['videoCount'],
-                "Общее количество просмотров": statistics['viewCount']
-            }
-            return channel_info
+            self.title = channel_data['title']
+            self.description = channel_data['description']
+            self.url = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
+            self.subscriber_count = statistics['subscriberCount']
+            self.video_count = statistics['videoCount']
+            self.view_count = statistics['viewCount']
         else:
-            return None
+            raise ValueError(f"Канал с id {self.__channel_id} не найден или произошла ошибка при получении данных.")
 
-    def print_info(self):
-        if self.channel_info:
-            print("Информация о канале:")
-            print(f"id канала: {self.channel_info['id канала']}")
-            print(f"Название канала: {self.channel_info['Название канала']}")
-            print(f"Описание: {self.channel_info['Описание канала']}")
-            print(f"Ссылка на канал: {self.channel_info['Ссылка на канал']}")
-            print(f"Количество подписчиков: {self.channel_info['Количество подписчиков']}")
-            print(f"Количество видео: {self.channel_info['Количество видео']}")
-            print(f"Общее количество просмотров: {self.channel_info['Общее количество просмотров']}")
-        else:
-            print("Канал не найден или произошла ошибка при получении данных.")
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print("Информация о канале:")
+        print(f"id канала: {self.__channel_id}")
+        print(f"Название канала: {self.title}")
+        print(f"Описание: {self.description}")
+        print(f"Ссылка на канал: {self.url}")
+        print(f"Количество подписчиков: {self.subscriber_count}")
+        print(f"Количество видео: {self.video_count}")
+        print(f"Общее количество просмотров: {self.view_count}")
 
-    def to_json(self, filename: str):
-        if self.channel_info:
-            with open(filename, 'w', encoding='utf-8') as file:
-                json.dump(self.channel_info, file, ensure_ascii=False, indent=2)
-                print(f"Данные канала сохранены в файл {filename}")
-        else:
-            print("Невозможно сохранить данные, так как канал не найден или произошла ошибка при получении данных.")
+    def to_json(self, filename: str) -> None:
+        """Сохраняет данные экземпляра класса в файл."""
+        dict_to_write = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count,
+        }
+        with open(filename, 'w') as fp:
+            json.dump(dict_to_write, fp)
 
     def __str__(self):
-        return f"{self.channel_info['Название канала']} ({self.channel_info['Ссылка на канал']})"
+        return f"{self.title} ({self.url})"
 
     def __add__(self, other):
         if isinstance(other, Channel):
-            return int(self.channel_info['Количество подписчиков']) + int(other.channel_info['Количество подписчиков'])
+            return int(self.subscriber_count) + int(other.subscriber_count)
         else:
             raise TypeError("Unsupported operand type for +: 'Channel' and {}".format(type(other)))
 
     def __sub__(self, other):
         if isinstance(other, Channel):
-            return int(self.channel_info['Количество подписчиков']) - int(other.channel_info['Количество подписчиков'])
+            return int(self.subscriber_count) - int(other.subscriber_count)
         else:
             raise TypeError("Unsupported operand type for -: 'Channel' and {}".format(type(other)))
 
     def __lt__(self, other):
         if isinstance(other, Channel):
-            return int(self.channel_info['Количество подписчиков']) < int(other.channel_info['Количество подписчиков'])
+            return int(self.subscriber_count) < int(other.subscriber_count)
         else:
             raise TypeError("Unsupported operand type for <: 'Channel' and {}".format(type(other)))
 
     def __ge__(self, other):
         if isinstance(other, Channel):
-            return int(self.channel_info['Количество подписчиков']) >= int(other.channel_info['Количество подписчиков'])
+            return int(self.subscriber_count) >= int(other.subscriber_count)
         else:
             raise TypeError("Unsupported operand type for >=: 'Channel' and {}".format(type(other)))
 
